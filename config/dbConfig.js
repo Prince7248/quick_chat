4e4ff9108efb83cb7232888d4aa7f7ce53a9d33f
APOORV VARSHNEY <apoorvvarshney8@gmail.com>
2024-11-30 10:57:14 +0530

    changes

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,18 +1,33 @@
-const mongoose= require('mongoose');
+// const mongoose= require('mongoose');
 
-//connection logic
-mongoose.connect(process.env.CONN_STRING);
+// //connection logic
+// mongoose.connect(process.env.CONN_STRING);
 
-//connection state
-const db = mongoose.connection;
+// //connection state
+// const db = mongoose.connection;
 
-//check DB connection
-db.on('connected', ()=>{
-    console.log('DB Connection Sucessfull!');
-});
-db.on('err', ()=>{
-    console.log('DB Connection failed');
+// //check DB connection
+// db.on('connected', ()=>{
+//     console.log('DB Connection Sucessfull!');
+// });
+// db.on('err', ()=>{
+//     console.log('DB Connection failed');
     
-});
+// });
 
-module.exports = db;
+// module.exports = db;
+
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+
+const connectDB= async()=>{
+     try{
+            await mongoose.connect('mongodb://localhost:27017/quick-chat')
+            console.log(`MongoDB Connected ${mongoose.connection.host}`);
+     }catch(error){
+        console.log(`MongoDB Error ${error}`);
+     }
+}
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,11 +1,18 @@
-const dotenv = require('dotenv');
-dotenv.config({path: './config.env'});
+// const dotenv = require('dotenv');
+// dotenv.config({path: './config.env'});
 
-const dbconfig = require('./config/dbConfig');
+const dotenv = require('dotenv').config();
+const connectDB = require('./config/dbConfig');
+
+// const dbconfig = require('./config/dbConfig');
 
 const app = require('./app');
 
-const port = process.env.PORT_NUMBER || 8001;
+// database connect
+connectDB();
+
+// const port = process.env.PORT_NUMBER || 8001;
+const port = 8001;
 
 app.listen(port,() =>{
     console.log('Listening to request on PORT:' + port);
